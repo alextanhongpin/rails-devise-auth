533aa9c92c0633acf17bf04e60f4fc4f27132853
alextanhongpin <alextan220990@gmail.com>
2019-09-26 01:34:19 +0800

    chore: add sample error logger

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   end
 
   def health
+    logger.error "hello", name: "john", age: 10
     render json: {
       version: Git::VERSION,
       revision: Git::REVISION
